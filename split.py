62c7646b822d1bfa2df4db21b45509956c59603f
Erin Kim <erinkim395@gmail.com>
2022-05-03 20:44:47 -0400

    add train test split class

---
+++ split.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import train_test_split
+
+
+class Split():
+    
+    def __init__(self, X_df, y_df, train_size, random_state):
+        self.X_df = X_df
+        self.y_df = y_df
+        self.train_size = train_size
+        self.random_state = random_state
+
+    def call(self):
+        return train_test_split(
+            self.X_df, 
+            self.y_df, 
+            self.train_size, 
+            self.random_state
+        )
